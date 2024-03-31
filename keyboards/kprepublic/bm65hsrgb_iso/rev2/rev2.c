fd30fbf168ae4cafe097c32b1f51a2b7d57d3a22
plodah <mleiper89@gmail.com>
2024-03-31 12:49:43 +0100

    Add BM65 rev2 & BM65 rev2 ISO

---
+++ keyboards/kprepublic/bm65hsrgb_iso/rev2/rev2.c
@@ -0,0 +1,171 @@
+#include "quantum.h"
+#ifdef RGB_MATRIX_ENABLE
+#    include "i2c_master.h"
+#    include "is31fl3741.h"
+#    include "ws2812.h"
+
+// clang-format off
+const is31fl3741_led_t PROGMEM g_is31fl3741_leds[is31fl3741_LED_COUNT] =
+    {0, CS6_SW1, CS5_SW1, CS4_SW1},
+    {0, CS6_SW2, CS5_SW2, CS4_SW2},
+    {0, CS6_SW3, CS5_SW3, CS4_SW3},
+    {0, CS6_SW4, CS5_SW4, CS4_SW4},
+    {0, CS6_SW5, CS5_SW5, CS4_SW5},
+    {0, CS6_SW6, CS5_SW6, CS4_SW6},
+    {0, CS6_SW7, CS5_SW7, CS4_SW7},
+    {0, CS6_SW8, CS5_SW8, CS4_SW8},
+    {0, CS18_SW1, CS17_SW1, CS16_SW1},
+    {0, CS18_SW2, CS17_SW2, CS16_SW2},
+    {0, CS18_SW3, CS17_SW3, CS16_SW3},
+    {0, CS18_SW4, CS17_SW4, CS16_SW4},
+    {0, CS18_SW5, CS17_SW5, CS16_SW5},
+    {0, CS18_SW6, CS17_SW6, CS16_SW6},
+    {0, CS18_SW7, CS17_SW7, CS16_SW7},
+
+    {0, CS9_SW1, CS8_SW1, CS7_SW1},
+    {0, CS9_SW2, CS8_SW2, CS7_SW2},
+    {0, CS9_SW3, CS8_SW3, CS7_SW3},
+    {0, CS9_SW4, CS8_SW4, CS7_SW4},
+    {0, CS9_SW5, CS8_SW5, CS7_SW5},
+    {0, CS9_SW6, CS8_SW6, CS7_SW6},
+    {0, CS9_SW7, CS8_SW7, CS7_SW7},
+    {0, CS9_SW8, CS8_SW8, CS7_SW8},
+    {0, CS21_SW1, CS20_SW1, CS19_SW1},
+    {0, CS21_SW2, CS20_SW2, CS19_SW2},
+    {0, CS21_SW3, CS20_SW3, CS19_SW3},
+    {0, CS21_SW4, CS20_SW4, CS19_SW4},
+    {0, CS21_SW5, CS20_SW5, CS19_SW5},
+    {0, CS21_SW7, CS20_SW7, CS19_SW7},
+
+    {0, CS12_SW1, CS11_SW1, CS10_SW1},
+    {0, CS12_SW2, CS11_SW2, CS10_SW2},
+    {0, CS12_SW3, CS11_SW3, CS10_SW3},
+    {0, CS12_SW4, CS11_SW4, CS10_SW4},
+    {0, CS12_SW5, CS11_SW5, CS10_SW5},
+    {0, CS12_SW6, CS11_SW6, CS10_SW6},
+    {0, CS12_SW7, CS11_SW7, CS10_SW7},
+    {0, CS12_SW8, CS11_SW8, CS10_SW8},
+    {0, CS24_SW1, CS23_SW1, CS22_SW1},
+    {0, CS24_SW2, CS23_SW2, CS22_SW2},
+    {0, CS24_SW3, CS23_SW3, CS22_SW3},
+    {0, CS24_SW4, CS23_SW4, CS22_SW4},
+    {0, CS24_SW5, CS23_SW5, CS22_SW5},
+    {0, CS21_SW6, CS20_SW6, CS19_SW6},
+    {0, CS24_SW7, CS23_SW7, CS22_SW7},
+
+    {0, CS15_SW1, CS14_SW1, CS13_SW1},
+    {0, CS15_SW2, CS14_SW2, CS13_SW2},
+    {0, CS15_SW3, CS14_SW3, CS13_SW3},
+    {0, CS15_SW4, CS14_SW4, CS13_SW4},
+    {0, CS15_SW5, CS14_SW5, CS13_SW5},
+    {0, CS15_SW6, CS14_SW6, CS13_SW6},
+    {0, CS15_SW7, CS14_SW7, CS13_SW7},
+    {0, CS15_SW8, CS14_SW8, CS13_SW8},
+    {0, CS27_SW1, CS26_SW1, CS25_SW1},
+    {0, CS27_SW2, CS26_SW2, CS25_SW2},
+    {0, CS27_SW3, CS26_SW3, CS25_SW3},
+    {0, CS27_SW4, CS26_SW4, CS25_SW4},
+    {0, CS27_SW5, CS26_SW5, CS25_SW5},
+    {0, CS27_SW6, CS26_SW6, CS25_SW6},
+    {0, CS27_SW7, CS26_SW7, CS25_SW7},
+
+    {0, CS3_SW1, CS2_SW1, CS1_SW1},
+    {0, CS3_SW2, CS2_SW2, CS1_SW2},
+    {0, CS3_SW3, CS2_SW3, CS1_SW3},
+    {0, CS3_SW7, CS2_SW7, CS1_SW7},
+    {0, CS30_SW3, CS29_SW3, CS28_SW3},
+    {0, CS30_SW4, CS29_SW4, CS28_SW4},
+    {0, CS30_SW5, CS29_SW5, CS28_SW5},
+    {0, CS30_SW6, CS29_SW6, CS28_SW6},
+    {0, CS30_SW7, CS29_SW7, CS28_SW7}
+
+};
+
+
+
+// Custom Driver
+static void init(void) {
+    i2c_init();
+
+    is31fl3731_init(0);
+    is31fl3731_init(1);
+
+    for (int index = 0; index < IS31FL3731_LED_COUNT; index++) {
+        bool enabled = !(   ( index == 18+5) || //B5
+                            ( index == 36+17) || //C17
+                            ( index == 54+13) //D13
+                        );
+        is31fl3731_set_led_control_register(index, enabled, enabled, enabled);
+    }
+
+    is31fl3731_update_led_control_registers(0);
+    is31fl3731_update_led_control_registers(1);
+}
+
+const rgb_matrix_driver_t rgb_matrix_driver = {
+    .init = init,
+    .flush = is31fl3731_flush,
+    .set_color = is31fl3731_set_color,
+    .set_color_all = is31fl3731_set_color_all
+};
+
+// clang-format on
+// ==========================================================================
+// Custom RGB Matrix driver that combines is31fl3741 and WS2812
+// ==========================================================================
+
+#    if WS2812_LED_TOTAL > 0
+rgb_led_t rgb_matrix_ws2812_array[WS2812_LED_TOTAL];
+#    endif
+
+static void rgb_matrix_driver_init(void) {
+    i2c_init();
+    is31fl3741_init(0);
+    for (uint8_t index = 0; index < IS31FL3741_LED_COUNT; index++) {
+        bool enabled = true;
+        is31fl3741_set_led_control_register(index, enabled, enabled, enabled);
+    }
+    is31fl3741_update_led_control_registers(0);
+}
+
+static void rgb_matrix_driver_flush(void) {
+    is31fl3741_update_pwm_buffers(0);
+#    if WS2812_LED_TOTAL > 0
+    ws2812_setleds(rgb_matrix_ws2812_array, WS2812_LED_TOTAL);
+#    endif
+}
+
+static void rgb_matrix_driver_set_color(int index, uint8_t red, uint8_t green, uint8_t blue) {
+    if (index < IS31FL3741_LED_COUNT) {
+        is31fl3741_set_color(index, red, green, blue);
+    } else {
+#    if WS2812_LED_TOTAL > 0
+        rgb_matrix_ws2812_array[index - IS31FL3741_LED_COUNT].r = red;
+        rgb_matrix_ws2812_array[index - IS31FL3741_LED_COUNT].g = green;
+        rgb_matrix_ws2812_array[index - IS31FL3741_LED_COUNT].b = blue;
+#    endif
+    }
+}
+
+static void rgb_matrix_driver_set_color_all(uint8_t red, uint8_t green, uint8_t blue) {
+    is31fl3741_set_color_all(red, green, blue);
+#    if WS2812_LED_TOTAL > 0
+    for (uint8_t i = 0; i < WS2812_LED_TOTAL; i++) {
+        rgb_matrix_ws2812_array[i].r = red;
+        rgb_matrix_ws2812_array[i].g = green;
+        rgb_matrix_ws2812_array[i].b = blue;
+    }
+#    endif
+}
+
+// clang-format off
+const rgb_matrix_driver_t rgb_matrix_driver = {
+    .init          = rgb_matrix_driver_init,
+    .flush         = rgb_matrix_driver_flush,
+    .set_color     = rgb_matrix_driver_set_color,
+    .set_color_all = rgb_matrix_driver_set_color_all,
+};
+// clang-format on
+
+
+#endif //RGB_MATRIX_ENABLE

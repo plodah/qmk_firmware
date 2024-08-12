a100ae53691663a68871f975c2a64e300129a22d
plodah <mleiper89@gmail.com>
2024-08-13 00:56:53 +0100

    fix starlight animations

--- quantum/rgb_matrix/animations/starlight_dual_hue_anim.h
+++ quantum/rgb_matrix/animations/starlight_dual_hue_anim.h
@@ -2,7 +2,8 @@
 RGB_MATRIX_EFFECT(STARLIGHT_DUAL_HUE)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-void set_starlight_dual_hue_color(uint8_t i, effect_params_t* params) {
+void set_starlight_dual_hue_color(int i, effect_params_t* params) {
+    if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) return;
     uint16_t time = scale16by8(g_rgb_timer, rgb_matrix_config.speed / 8);
     HSV      hsv  = rgb_matrix_config.hsv;
     hsv.v         = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
@@ -12,18 +13,16 @@ void set_starlight_dual_hue_color(uint8_t i, effect_params_t* params) {
 }
 
 bool STARLIGHT_DUAL_HUE(effect_params_t* params) {
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
     if (!params->init) {
         if (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 5)) % 5 == 0) {
             uint8_t rand_led = random8_max(RGB_MATRIX_LED_COUNT);
             set_starlight_dual_hue_color(rand_led, params);
         }
-        return false;
-    }
-
-    RGB_MATRIX_USE_LIMITS(led_min, led_max);
-    for (uint8_t i = led_min; i < led_max; i++) {
-        RGB_MATRIX_TEST_LED_FLAGS();
-        set_starlight_dual_hue_color(i, params);
+    } else {
+        for (int i = led_min; i < led_max; i++) {
+            set_starlight_dual_hue_color(i, params);
+        }
     }
     return rgb_matrix_check_finished_leds(led_max);
 }

6a504512c6aef9ed85f48961e16a38fd0348bfc5
plodah <mleiper89@gmail.com>
2024-12-13 01:06:23 +0000

    better place

--- keyboards/ploopyco/ploopyco.c
+++ keyboards/ploopyco/ploopyco.c
@@ -127,13 +127,6 @@ void encoder_driver_task(void) {
 }
 #endif
 
-bool led_update_kb(led_t led_state) {
-#if defined(PLOOPY_DRAGSCROLL_ON_SCROLLLOCK_ENABLED)
-    is_drag_scroll = led_state.scroll_lock;
-#endif // PLOOPY_DRAGSCROLL_ON_SCROLLLOCK_ENABLED
-    return true;
-}
-
 #ifndef PAVONIS
 void toggle_drag_scroll(void) {
     is_drag_scroll ^= 1;
@@ -244,6 +237,13 @@ void pointing_device_init_kb(void) {
         eeconfig_init_kb();
     }
     pointing_device_set_cpi(dpi_array[keyboard_config.dpi_config]);
+}
+
+bool led_update_kb(led_t led_state) {
+#if defined(PLOOPY_DRAGSCROLL_ON_SCROLLLOCK_ENABLED)
+    is_drag_scroll = led_state.scroll_lock;
+#endif // PLOOPY_DRAGSCROLL_ON_SCROLLLOCK_ENABLED
+    return true;
 }
 
 void eeconfig_init_kb(void) {
